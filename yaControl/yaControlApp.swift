9640623a0b7a91cc31ea4a1f9f4210b32dbd91ce
LeChat <sedoykin@gmail.com>
2025-02-17 19:40:20 +0300

    Initial Commit

---
+++ yaControl/yaControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  yaControlApp.swift
+//  yaControl
+//
+//  Created by Sedoykin Alexey on 17/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct yaControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
